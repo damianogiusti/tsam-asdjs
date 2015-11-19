783050fab5e3c847a208645423778ec56636e193
damianogiusti <damianogiusti95@gmail.com>
2015-11-19 16:28:26 +0100

    auto-push script by dg

--- homework_03.js
+++ homework_03.js
@@ -5,236 +5,34 @@ Dato un array di interi, restituire la loro somma fino a che non viene ritrovato
 negativo.
 */
 
-// sum con la every
-function sum1(a) {
-    var tot = 0;
-    a.every(x => {
-        if (x >= 0)
-            tot += x;
-        return x > 0;
-    });
+Array.prototype.sum3 = function () {
 
-    return tot;
-}
-
-// sum con every e reduce
-function sum2(a) {
-    var tmparray = [];
-    a.every(x => {
-        if (x >= 0)
-            tmparray.push(x)
-        return x > 0;
-    });
-
-    return tmparray.reduce((acc, x) => acc + x, 0);
-}
-
-/**
-* costruisce un array basato su myarray,
-* e come elementi inserisce quelli di myarray finchè
-* la funzione check è verificata
-*/
-function buildUntil(myarray, check) {
-    var tmparray = [];
-    myarray.every(x => {
-        if (check(x)) {
-            tmparray.push(x);
-            return true;
-        } else {
-            return false;
-        }
-    });
-    return tmparray;
-}
-
-// sum con buildUntil
-function sum3(a) {
-    return buildUntil(a, x => x > 0).reduce((acc, x) => acc + x, 0);
-}
-
-// funzione sum fatta da cagnone
-function sum_ingnorantissima(a) {
-    var trovato = false;
-    var array = a.map(
-        function(x) {
-            if (x < 0) 
-                trovato = true;
-            if (!trovato)
-                return x;
-            return 0;
+    /**
+    * costruisce un array basato su myarray,
+    * e come elementi inserisce quelli di myarray finchè
+    * la funzione check è verificata
+    */
+    function buildUntil(a, check) {
+        var tmparray = [];
+        a.every(x => {
+            if (check(x)) {
+                tmparray.push(x);
+                return true;
+            } else {
+                return false;
+            }
         });
-    
-    return array.reduce(
-        function (acc, x) {
-            return acc + x;
-        });
-}
-
-// Per Tiziano
-function ex_1_F(a) {
-    return sum3(a);
-}
-
-/********************************************************************* 
-
-Esercizio 2
-
-Dato un numero n, restituire la somma dei primi n numeri interi positivi dispari
-*/
-
-// O(n)
-function sumn(n) {
-    return range(n).map(x => 2*x+1).reduce((acc, x) => acc + x);
-}
-
-function range(n) {
-    a = [];
-    for (var i=0; i<n; i++)
-        a[i] = i;
-
-    return a;
-}
-
-// Per Tiziano
-function ex_2_F(x) {
-    return sumn(x);
-}
-
-/******************************************************************* 
-
-Esercizio 3
-
-Dato un array di 10 elementi, calcolarne la media
-*/
-
-// O(n)
-
-function avg(a) {
-    return a.reduce((acc, x) => acc + x/a.length, 0);
-}
-
-// Per Tiziano
-function ex_3_F(a) {
-    return avg(a);
-}
-
-/***************************************************************************
-
- Esercizio 4
-
-Dato un intervallo [a, b] con a e b due interi positivi, restituire la somma di tutti i numeri
-
-compresi all’interno dell’intervallo, estremi inclusi. Nel caso che b fosse minore di a,
-
-calcolare la somma nell’intervallo [b,a]
-
-Esempio:
-
-sumInterval(3, 5) => 12
-
-sumInterval(5, 3) => 12
-*/
-
-// O(n)
-
-function sumInterval(a, b) {
-
-    function intervallo(a, b) {
-        var array = [];
-        for (var i=Math.min(a,b); i<=Math.max(a,b); i++) {
-            array.push(i);
-        }
-        return array;
+        return tmparray;
     }
 
-    return intervallo(a, b).reduce((acc, x) => acc + x);
+    return buildUntil(this, x => x > 0).reduce((acc, x) => acc + x, 0);
 }
 
 // Per Tiziano
-function ex_4_F(a, b) {
-    return sumInterval(a, b);
+function ex_1_P(a) {
+    return a.sum3();
 }
 
-/***************************************************************************
-
- Esercizio 5
-
-Si calcoli il prodotto di due numeri a, b maggiori o uguali a zero, tramite l’utilizzo del solo
-
-operatore somma.
-
-Esempio:
-
-mult(2, 3) => 6
-*/
-
-// O(n)
-
-function mult(a, b) {
-    return replicate(a,b).reduce((acc, x) => acc + x);
-}
-
-// Per Tiziano
-function ex_5_F(a, b) {
-    return mult(a, b);
-}
-
-/***************************************************************************
-
- Esercizio 6
-
-Si calcoli la divisione e il resto della divisione tra due numeri a, b maggiori a zero, tramite
-
-l’utilizzo dei soli operatori somma e sottrazione.
-
-Esempio:
-
-div(5, 3) => 1 resto 2
-*/
-
-function div(a, b) {
-
-    // div(5,3)
-    // replicate(a, a) => [5,5,5,5,5]
-    var temp = 0;
-    var array = replicate(a, a).map((x) => {
-        var local = x - b - temp;   // local = 5-3-temp
-        temp += b;                  // temp accumula 3
-        return local;               // local diventa un elemento del nuovo array
-    }).filter(x => x >= 0);         // elimino dall'array gli elementi negativi
-
-    // array.length     => quoziente
-    // array[lung-1]    => resto
-
-    return [array.length, array[array.length -1]];
-}
-
-// Per Tiziano
-function ex_6_F(a, b) {
-    return div(a, b);
-}
-/***************************************************************************
-
- Esercizio 7
-
-Si calcoli la potenza (x^y) di due numeri x y maggiori o uguali a zero, tramite l’utilizzo dei soli
-
-operatori somma, sottrazione e della funzione mult.
-
-Esempio:
-
-pow(2, 3) => 8
-
-*/
-
-function pow(a, b) {
-    return replicate(a, b).reduce((acc, x) => mult(acc, x), 1);
-}
-
-// Per Tiziano
-function ex_7_F(a, b) {
-    return pow(a, b);
-}
 /***************************************************************************
 
  Esercizio 8
